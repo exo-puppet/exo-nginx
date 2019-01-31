a011092516425c52ecf666591899bb502690e832
Frédéric DROUET <fdrouet@exoplatform.com>
2019-01-31 16:09:19 +0100

    Improve default configuration
    - default docker open port is now only 80 (443 not by default)
    - ability to activate server-status endpoint on port 81
      - no more enabled by default
      - no more enabled on default vhost on port 80
    - the default vhost now reject requests with code 444 (only bad requests should arrive on it)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,19 @@
 # nginx
 class nginx (
-  $image            = 'nginx',
-  $version          = '1.14.2',
-  $container_name   = 'nginx',
+  $image                 = 'nginx',
+  $version               = '1.14.2',
+  $container_name        = 'nginx',
+  $docker_ports          = ['80:80'],
   $network,
-  $install_dir      = '/opt/nginx',
-  $cert_dir         = '/opt/nginx/sslcert',
-  $log_dir          = '/var/log/nginx',
-  $status_url       = '/server-status',
+  $install_dir           = '/opt/nginx',
+  $cert_dir              = '/opt/nginx/sslcert',
+  $log_dir               = '/var/log/nginx',
+  $monitoring_enabled    = false,
+  $monitoring_status_url = '/server_status',
 ) {
-  $nginx_uid        = 104
-  $nginx_gid        = 107
-  $conf_dir         = "${install_dir}/conf.d"
+  $nginx_uid = 104
+  $nginx_gid = 107
+  $conf_dir  = "${install_dir}/conf.d"
 
   file { $install_dir :
     ensure => directory,
@@ -56,11 +58,28 @@ class nginx (
     require => File[$conf_dir],
     notify  => Docker::Run['nginx'],
   }
+  file { "${conf_dir}/100_monitoring.conf" :
+    ensure  => $monitoring_enabled ? {
+      true    => present,
+      default => absent
+    },
+    owner   => $nginx_uid,
+    group   => $nginx_gid,
+    mode    => '0644',
+    content => template('nginx/conf/monitoring.conf.erb'),
+    require => File[$conf_dir],
+    notify  => Docker::Run['nginx'],
+  }
 
+  if $monitoring_enabled {
+    $_docker_ports = concat($docker_ports , ['127.0.0.1:81:81'])
+  } else {
+    $_docker_ports = $docker_ports
+  }
   docker::run { $container_name :
     image    => "${image}:${version}",
     hostname => 'nginx',
-    ports    => ['80:80','443:443'],
+    ports    => $_docker_ports,
     volumes  => [
       "${conf_dir}:/etc/nginx/conf.d",
       "${log_dir}:/var/log/nginx",
